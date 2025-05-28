7199a3b875af232927accbee0685e1b2150307ef
elroi773 <s2457@e-mirim.hs.kr>
2025-05-28 11:19:55 +0900

    index.vue 하는중

--- np-project/src/main.js
+++ np-project/src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router' // 라우터 불러오기
+
 
 createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(router)
+app.mount('#app')
