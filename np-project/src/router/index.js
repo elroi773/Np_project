14d6a2b72555d0b5101bde6bc75f0da1d0d30f5e
elroi773 <s2457@e-mirim.hs.kr>
2025-05-28 11:20:03 +0900

    index.vue 하는중

---
+++ np-project/src/router/index.js
@@ -0,0 +1,18 @@
+// src/router/index.js
+import { createRouter, createWebHistory } from 'vue-router'
+import Index from '@/views/Index.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Index
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
