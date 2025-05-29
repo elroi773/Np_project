b77300364cfac8230beb755e47d458fe79111449
elroi773 <s2457@e-mirim.hs.kr>
2025-05-29 22:53:38 +0900

    .

--- np-project/src/router/index.js
+++ np-project/src/router/index.js
@@ -1,13 +1,21 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router'
 import Index from '@/views/Index.vue'
+import Join1 from '../views/join1.vue' 
+
+
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Index
-  }
+  },
+  { 
+    path: '/join1', 
+    name: 'Join1', 
+    component: Join1 
+  } 
 ]
 
 const router = createRouter({
