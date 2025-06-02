1832baecaa6a9096ef744fa87421a399f23bb31e
elroi773 <s2457@e-mirim.hs.kr>
2025-06-02 12:19:31 +0900

    index

--- np-project/src/views/index.vue
+++ np-project/src/views/index.vue
@@ -2,7 +2,7 @@
     <div class="container">
       <img src="@/assets/logo.png" alt="오늘의 땡처리 로고" class="logo">
       <div class="button-group">
-        <button class="btn"><a href="#">로그인 하기</a></button>
+        <button class="btn" @click="goToLogin"><a href="#">로그인 하기</a></button>
         <button class="btn" @click="goToJoin">회원가입 하기</button>
       </div>
     </div>
@@ -14,8 +14,14 @@
       goToJoin() {
         this.$router.push('/join1');
       }
+    },methods: {
+      goToLogin() {
+        this.$router.push('/login');
+      }
     }
+
   }
+
   </script>
   
 
