55cf0b70ff3092df743813bdb0dde9efc4081e88
elroi773 <s2457@e-mirim.hs.kr>
2025-06-09 10:18:44 +0900

    가입 완료 페이지

---
+++ backend/np-project/src/views/join_complete.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+      가입 완료 페이지입니다.
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'JoinComplete'
+  }
+  </script>
+  
+  <style scoped>
+  /* 필요 시 스타일 작성 */
+  </style>
+  
