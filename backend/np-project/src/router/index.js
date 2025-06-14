a9e7ced3810a3741d6e05812cebcffd8ab2e3a8f
elroi773 <s2457@e-mirim.hs.kr>
2025-06-14 22:33:30 +0900

    db 연결 성공 ㅎ

--- backend/np-project/src/router/index.js
+++ backend/np-project/src/router/index.js
@@ -12,6 +12,7 @@ import Main_sell from '@/views/main_sell.vue'
 import Join2_Sell from '@/views/join2_sell.vue'
 import Join3_Sell from '@/views/join3_sell.vue'
 import Join4_Sell from '@/views/join4_sell.vue'
+import Join5_Sell from '@/views/join5_sell.vue'
 import Join_Complete from '@/views/join_complete.vue'
 import Join_Complete_Sell from '@/views/join_complete_sell.vue'
 import Notifications from '@/views/notifications.vue'
@@ -33,6 +34,7 @@ const routes = [
   { path: '/join2_sell', name: 'Join2Sell', component: Join2_Sell },
   { path: '/join3_sell', name: 'Join3Sell', component: Join3_Sell },
   { path: '/join4_sell', name: 'Join4Sell', component: Join4_Sell },
+  { path: '/join5_sell', name: 'Join5Sell', component: Join5_Sell },
   { path: '/join_complete', name: 'JoinComplete', component: Join_Complete },
   { path: '/join_complete_sell', name: 'JoinCompleteSell', component: Join_Complete_Sell },
   { path: '/profile', name: 'Profile', component: Profile },
